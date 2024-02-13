c105575a4bf1ae5e59210f727b0c6f0b23473c05
aryanjnv <aryangpjnv188@gmail.com>
2024-02-13 16:36:52 +0530

    complete

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header/Header';
+import Input from './Input/Input';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Input/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Input/MedicineList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const MedicineList = (props) => {
+  return (
+    <div>
+      {  props.medicineList.map((item)=>{
+         return <li><span>{item.medicineName}</span>--
+         <span>{item.description}</span>--
+         <span>{item.price}</span>
+         <input min='1' max='5' defaultValue='1' type="number" />
+         <button>+ Add To Cart</button></li>
+        })}
+
+    </div>
+  )
+}
+
+export default MedicineList
